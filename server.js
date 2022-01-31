cf28e30b63ffec4cd9026d4104199144db524377
s.pera6@gmail.com <s.pera6@gmail.com>
2022-01-31 09:40:20 -0700

    setup basic express backend and added dependencies, comments, and .env/.gitignore

---
+++ server.js
@@ -0,0 +1,25 @@
+//  DEPENDENCIES
+
+
+// Initalize the express app
+
+
+// configuring server settings
+
+
+// expose our config variables
+
+
+// connect to mongoDB
+
+
+// set up our mongoDB event listeners
+
+
+// mount middleware
+
+
+// routes
+
+
+// tell the app to listen
