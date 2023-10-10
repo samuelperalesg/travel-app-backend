1c4f48246f2276b2fbd81d7e1c96d4f4aca47582
samuelperalesg <91507521+samuelperalesg@users.noreply.github.com>
2023-10-10 16:40:12 -0600

    wip: updating routes

--- routes/travelRoutes.js
+++ routes/travelRoutes.js
@@ -22,26 +22,24 @@ router.get("/locations", isAuthenticated, async (req, res, next) => {
 
 // create route
 router.post("/locations", isAuthenticated, async (req, res, next) => {
-  console.log("Received data:", req.body);
 
   if (!req.body.name || !req.body.image) {
-      return res.status(400).send("Name and Image are required.");
+    return res.status(400).send("Name and Image are required.");
   }
 
   const locationToSave = {
-      ...req.body,
-      uid: req.user.uid, // Add the user's UID to the location data
+    ...req.body,
+    uid: req.user.uid, // Add the user's UID to the location data
   };
 
   try {
-      const savedLocation = await Travel.create(locationToSave);
-      res.json(savedLocation);
+    const savedLocation = await Travel.create(locationToSave);
+    res.json(savedLocation);
   } catch (error) {
     console.error("Database Error:", error);
     res.status(400).json(error);
-}
+  }
 });
-
 
 // update route
 router.put("/locations/:id", isAuthenticated, async (req, res, next) => {
@@ -65,20 +63,28 @@ router.put("/locations/:id", isAuthenticated, async (req, res, next) => {
   }
 });
 
-// delete
+// delete route
 router.delete("/locations/:id", isAuthenticated, async (req, res, next) => {
   try {
     const location = await Travel.findById(req.params.id);
-    if (!location) return res.status(404).send("Location not found");
+
+    // Check if the location with the provided ID exists
+    if (!location) {
+      return res.status(404).send("Location not found");
+    }
 
     // Check if the UID in the entry matches the UID from the token
     if (location.uid !== req.user.uid) {
       return res.status(403).send("Permission denied");
     }
 
-    res.json(await Travel.findByIdAndDelete(req.params.id));
+    // Attempt to delete the location
+    await location.remove();
+
+    res.json({ message: "Location deleted successfully", id: req.params.id });
   } catch (error) {
-    res.status(400).json(error);
+    console.error("Error in DELETE /locations/:id:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
